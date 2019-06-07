5645224043116da9e98e1e3fd94f19925d8e50df
unknown <holdcroftkari@gmail.com>
2019-06-07 21:27:56 +0200

    Added slides logic

--- js/slides.js
+++ js/slides.js
@@ -1,30 +1,127 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+const slideImages = [
+    {
+        path: './images/lifeonmars.jpg',
+        alt: 'life on mars',
+        text: 'Future living on mars'
+    },
+    {
+        path: './images/first_reflight.jpg',
+        alt: 'first reflight',
+        text: 'Reflight'
+    },
+    {
+        path: './images/artificial-intelligence-astronomy.jpg',
+        alt: 'Artificial intelligence on mars',
+        text: 'AI assisted living!'
+    },
+    {
+        path: './images/MarsGroundHabitat.jpg',
+        alt: 'mars ground habitat',
+        text: 'Habitat on mars'
+    },
+    {
+        path: './images/Martian_habitat_with_colonists.jpg',
+        alt: 'mars habitat with colonists',
+        text: 'Habitat for the future colonists!'
+    },
+];
 
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+const slide = document.querySelector('.slide');
+const numberText = document.querySelector('.number-text');
+const caption = document.querySelector('#caption')
+const amountOfSlides = slideImages.length;
+const lastSlideIndex = (amountOfSlides - 1);
+const thumbnails = document.querySelector('#thumbnails');
+
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+
+let slideIndex = 0;
+
+initThumbnails();
+getSlideImage(slideImages[0]);
+
+next.addEventListener('click', function () {
+
+    (slideIndex !== lastSlideIndex) ? slideIndex++ : slideIndex = 0;
+
+    const slideToShow = slideImages[slideIndex];
+
+    getSlideImage(slideToShow);
+
+});
+
+prev.addEventListener('click', function () {
+
+    (slideIndex !== 0) ? slideIndex-- : slideIndex = lastSlideIndex;
+
+    const slideToShow = slideImages[slideIndex];
+
+    getSlideImage(slideToShow);
+
+});
+
+//Slider specific functions
+function getSlideImage(slideImage) {
+
+    if (slide.lastChild.className === 'image-slide') slide.removeChild(slide.lastChild);
+
+    const slideImageMarkup = getSlideImageMarkup(slideImage);
+
+    slide.appendChild(slideImageMarkup);
+
+    currenImageSlide = slideImageMarkup;
+
+    caption.innerText = slideImage.text;
+
+    numberText.innerText = `${slideImages.indexOf(slideImage) + 1} / ${amountOfSlides}`;
+
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+function getSlideImageMarkup(image) {
+    const imageElement = document.createElement('img');
+
+    imageElement.className = 'image-slide';
+
+    imageElement.src = image.path;
+
+    imageElement.alt = image.alt;
+
+    return imageElement;
 }
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+function initThumbnails() {
+
+    let idx = 0;
+
+    for (idx; idx < amountOfSlides; idx++) {
+
+        const slideImage = slideImages[idx];
+
+        const thumbnail = document.createElement('div');
+
+        thumbnail.className = 'column-lifeonmars';
+
+        thumbnail.innerHTML = `<img class="thumbnail" src="${slideImage.path}"  alt="${slideImage.alt}" />`;
+
+        thumbnail.addEventListener('click', function (event) {
+
+            const clickedElement = event.target;
+
+            const activeElement = thumbnails.querySelector('.active');
+
+            if (activeElement) {
+                activeElement.classList.remove('active');
+            }
+
+            clickedElement.classList.add('active');
+
+            getSlideImage(slideImage);
+        });
+
+        thumbnails.appendChild(thumbnail);
+
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
+
 }
+
